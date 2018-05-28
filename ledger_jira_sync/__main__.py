85ace7ebfadaa2a70243c94a0c0002693a77353a
Kim Rinnewitz <krinnewitz@plapadoo.de>
2018-05-28 12:44:02 +0200

    Remove development notes

--- ledger_jira_sync/__main__.py
+++ ledger_jira_sync/__main__.py
@@ -2,12 +2,6 @@
 import argparse
 from ledger_jira_sync.core import sync_ledger_jira
 
-# Parameter: JIRA-Login-Email, JIRA-Passwort, Ledger-Tag-Nutzername, Pfad zu Assoziationsdatei, Soll gelöscht werden? Default: ja
-# Konfigurierbar ob mit Kommentar oder ohne in JIRA gepostet werden soll pro Ticket. Default: Ohne Kommentare
-
-jira_email = 'krinnewitz@plapadoo.de'
-ledger_username = 'Kim'
-
 parser = argparse.ArgumentParser(description='Synchronize JIRA and ledger')
 parser.add_argument('--server', required=True, help='JIRA server to connect to')
 parser.add_argument('--email', required=True, help='JIRA e-mail to use for log-in')
